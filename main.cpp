85cdcaec1cc0d692c8783440c0836d6a704310e9
Ramchandra Joshi <rbjoshi1999@gmail.com>
2020-05-14 12:18:43 +0530

    removed macros

--- main.cpp
+++ main.cpp
@@ -1,40 +1,24 @@
 #include <bits/stdc++.h>
-#define show(x) cout << #x << ": " << x << endl
-#define ll long long
-#define fst first
-#define snd second
-#define pb push_back
-#define mp make_pair
-#define fori(ini, end, num) for (ll i = ini; i < end; i += num)
-#define forj(ini, end, num) for (ll j = ini; j < end; j += num)
-#define vll vector<ll>
-#define pr pair<ll, ll>
-#define um unordered_map
-#define all(v) v.begin(), v.end()
-#define allr(v) v.rbegin(), v.rend()
-#define light ios_base::sync_with_stdio(false);
-#define bolt cin.tie(NULL); cout.tie(NULL);
-#define endl "\n"
-#define mod 1000000007
 using namespace std;
 
-ll get_number_of_files()
+long long get_number_of_files()
 {
-    ll count = 0;
+    long long count = 0;
     string temp;
-    ifstream no_of_files("./data/file_names.txt", ios :: in);
+    ifstream no_of_files("./data/file_names.txt");
     while (no_of_files >> temp)
         ++count;
 
+    no_of_files.close();
     return count;
 }
 
 double idf(string term, vector<vector<string>> s)
 {
-    ll num_documents_with_term = 0;
-    fori (0, s.size(), 1)
+    long long num_documents_with_term = 0;
+    for (long long i = 0; i < s.size(); ++i)
     {
-        if (binary_search(all(s[i]), term) == true)
+        if (binary_search(s[i].begin(), s[i].end(), term) == true)
             ++num_documents_with_term;
     }
 
@@ -53,21 +37,21 @@ int main()
 
     vector<string> terms;
     string temp = "";
-    fori (0, s.size(), 1)
+    for (long long i = 0; i < s.size(); ++i)
     {
         if (s[i] == ' ')
         {
-            terms.pb(temp);
+            terms.push_back(temp);
             temp = "";
         }
         else
             temp += s[i];
     }
-    terms.pb(temp);
+    terms.push_back(temp);
 
-    ll total_given_words = terms.size();
+    long long total_given_words = terms.size();
 
-    ll total_no_of_files = get_number_of_files();
+    long long total_no_of_files = get_number_of_files();
     vector<string> file_names(total_no_of_files);
 
     vector<map<string, double>> normalized_tf_documents;
@@ -75,8 +59,8 @@ int main()
     set<string> total_unique_words;
 
     string files;
-    ll current_file = 0;
-    ifstream file_with_names("./data/file_names.txt", ios :: in);
+    long long current_file = 0;
+    ifstream file_with_names("./data/file_names.txt");
 
     while (file_with_names >> files)
     {
@@ -85,8 +69,8 @@ int main()
         vector<string> words_in_current_file;
         map<string, double> current_file_words;
         string words;
-        ll total_words_in_file = 0;
-        ifstream each_file("./data/" + files, ios :: in);
+        long long total_words_in_file = 0;
+        ifstream each_file("./data/" + files);
 
         while (each_file >> words)
         {
@@ -94,19 +78,19 @@ int main()
             ++total_words_in_file;
 
             total_unique_words.insert(words);
-            words_in_current_file.pb(words);
+            words_in_current_file.push_back(words);
         }
-        normalized_tf_documents.pb(current_file_words);
+        normalized_tf_documents.push_back(current_file_words);
 
         map<string, double>:: iterator itr;
         for (itr = normalized_tf_documents[current_file].begin(); itr != normalized_tf_documents[current_file].end(); ++itr)
         {
-            double count = itr->snd;
-            itr->snd = count / double(total_words_in_file);
+            double count = itr->second;
+            itr->second = count / double(total_words_in_file);
         }
 
-        sort(all(words_in_current_file));
-        words_in_each_file.pb(words_in_current_file);
+        sort(words_in_current_file.begin(), words_in_current_file.end());
+        words_in_each_file.push_back(words_in_current_file);
 
         each_file.close();
         ++current_file;
@@ -118,36 +102,36 @@ int main()
         inverse_document_frequency[i] = idf(i, words_in_each_file);
 
     vector<vector<double>> tf_idf_documents(total_given_words, vector<double>(total_no_of_files, 0));
-    fori (0, total_given_words, 1)
+    for (long long i = 0; i < total_given_words; ++i)
     {
         string current_word_from_input = terms[i];
-        forj (0, total_no_of_files, 1)
+        for (long long j = 0; j < total_no_of_files; ++j)
             tf_idf_documents[i][j] = normalized_tf_documents[j][current_word_from_input] * inverse_document_frequency[current_word_from_input];
     }
 
     map<string, double> normalized_tf_input;
-    fori (0, total_given_words, 1)
+    for (long long i = 0; i < total_given_words; ++i)
         normalized_tf_input[terms[i]] += 1;
 
     map<string, double>:: iterator input_words;
     for (input_words = normalized_tf_input.begin(); input_words != normalized_tf_input.end(); ++input_words)
     {
-        double count = input_words->snd;
-        input_words->snd = count / double(total_given_words);
+        double count = input_words->second;
+        input_words->second = count / double(total_given_words);
     }
 
     vector<double> tf_idf_input(total_given_words);
-    fori (0, total_given_words, 1)
+    for (long long i = 0; i < total_given_words; ++i)
     {
         string current_word_from_input = terms[i];
         tf_idf_input[i] = normalized_tf_input[current_word_from_input] * inverse_document_frequency[current_word_from_input];
     }
 
     vector<double> final_scores(total_no_of_files, 0);
-    fori (0, total_no_of_files, 1)
+    for (long long i = 0; i < total_no_of_files; ++i)
     {
         double magnitude_document = 0, magnitude_query = 0;
-        forj (0, total_given_words, 1)
+        for (long long j = 0; j < total_given_words; ++j)
         {
             final_scores[i] += (tf_idf_documents[j][i] * tf_idf_input[j]);
             magnitude_document += (tf_idf_documents[j][i] * tf_idf_documents[j][i]);
@@ -158,9 +142,9 @@ int main()
             final_scores[i] = final_scores[i] / (sqrt(magnitude_document) * sqrt(magnitude_query)); 
     }
 
-    double max_value = *max_element(all(final_scores));
+    double max_value = *max_element(final_scores.begin(), final_scores.end());
     cout << "\nRelevant Documents : " << endl;
-    fori (0, total_no_of_files, 1)
+    for (long long i = 0; i < total_no_of_files; ++i)
     {
         if (final_scores[i] == max_value)
             cout << file_names[i] << endl;
