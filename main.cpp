e428722614a2a1e25461260235663fc44dc08110
Ramchandra Joshi <rbjoshi1999@gmail.com>
2020-05-24 23:10:44 +0530

    input taken from command line

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long get_number_of_files()
+long long get_number_of_files(string file_name)
 {
     long long count = 0;
     string temp;
-    ifstream no_of_files("./data/file_names.txt");
+    ifstream no_of_files("./data/" + file_name);
     while (no_of_files >> temp)
         ++count;
 
@@ -28,126 +28,119 @@ double idf(string term, vector<vector<string>> s)
         return 1 + log(double(s.size()) / double(num_documents_with_term));
 }
 
-int main()
-{   
-    string s;
+int main(int argc, char* argv[])
+{
+    if (argc == 1)
+        cout << "Please Enter Filename and Query" << endl;
+    else if (argc == 2)
+        cout << "Please Enter Query" << endl;
+    else
+    {   
+        vector<string> terms;
+        for (long long i = 2; i < argc; ++i)
+            terms.push_back(argv[i]);
 
-    cout << "Enter Query : " << endl;
-    getline(cin, s);
+        long long total_given_words = argc - 2;
 
-    vector<string> terms;
-    string temp = "";
-    for (long long i = 0; i < s.size(); ++i)
-    {
-        if (s[i] == ' ')
+        string file_name = argv[1];
+        long long total_no_of_files = get_number_of_files(file_name);
+        vector<string> file_names(total_no_of_files);
+
+        vector<map<string, double>> normalized_tf_documents;
+        vector<vector<string>> words_in_each_file;
+        set<string> total_unique_words;
+
+        string files;
+        long long current_file = 0;
+        ifstream file_with_names("./data/" + file_name);
+
+        while (file_with_names >> files)
         {
-            terms.push_back(temp);
-            temp = "";
+            file_names[current_file] = files;
+
+            vector<string> words_in_current_file;
+            map<string, double> current_file_words;
+            string words;
+            long long total_words_in_file = 0;
+            ifstream each_file("./data/" + files);
+
+            while (each_file >> words)
+            {
+                current_file_words[words] += 1;
+                ++total_words_in_file;
+
+                total_unique_words.insert(words);
+                words_in_current_file.push_back(words);
+            }
+            normalized_tf_documents.push_back(current_file_words);
+
+            map<string, double>:: iterator itr;
+            for (itr = normalized_tf_documents[current_file].begin(); itr != normalized_tf_documents[current_file].end(); ++itr)
+            {
+                double count = itr->second;
+                itr->second = count / double(total_words_in_file);
+            }
+
+            sort(words_in_current_file.begin(), words_in_current_file.end());
+            words_in_each_file.push_back(words_in_current_file);
+
+            each_file.close();
+            ++current_file;
         }
-        else
-            temp += s[i];
-    }
-    terms.push_back(temp);
+        file_with_names.close();
 
-    long long total_given_words = terms.size();
+        map<string, double> inverse_document_frequency;
+        for (auto i : total_unique_words)
+            inverse_document_frequency[i] = idf(i, words_in_each_file);
 
-    long long total_no_of_files = get_number_of_files();
-    vector<string> file_names(total_no_of_files);
-
-    vector<map<string, double>> normalized_tf_documents;
-    vector<vector<string>> words_in_each_file;
-    set<string> total_unique_words;
-
-    string files;
-    long long current_file = 0;
-    ifstream file_with_names("./data/file_names.txt");
-
-    while (file_with_names >> files)
-    {
-        file_names[current_file] = files;
-
-        vector<string> words_in_current_file;
-        map<string, double> current_file_words;
-        string words;
-        long long total_words_in_file = 0;
-        ifstream each_file("./data/" + files);
-
-        while (each_file >> words)
+        vector<vector<double>> tf_idf_documents(total_given_words, vector<double>(total_no_of_files, 0));
+        for (long long i = 0; i < total_given_words; ++i)
         {
-            current_file_words[words] += 1;
-            ++total_words_in_file;
-
-            total_unique_words.insert(words);
-            words_in_current_file.push_back(words);
-        }
-        normalized_tf_documents.push_back(current_file_words);
-
-        map<string, double>:: iterator itr;
-        for (itr = normalized_tf_documents[current_file].begin(); itr != normalized_tf_documents[current_file].end(); ++itr)
-        {
-            double count = itr->second;
-            itr->second = count / double(total_words_in_file);
-        }
-
-        sort(words_in_current_file.begin(), words_in_current_file.end());
-        words_in_each_file.push_back(words_in_current_file);
-
-        each_file.close();
-        ++current_file;
-    }
-    file_with_names.close();
-
-    map<string, double> inverse_document_frequency;
-    for (auto i : total_unique_words)
-        inverse_document_frequency[i] = idf(i, words_in_each_file);
-
-    vector<vector<double>> tf_idf_documents(total_given_words, vector<double>(total_no_of_files, 0));
-    for (long long i = 0; i < total_given_words; ++i)
-    {
-        string current_word_from_input = terms[i];
-        for (long long j = 0; j < total_no_of_files; ++j)
-            tf_idf_documents[i][j] = normalized_tf_documents[j][current_word_from_input] * inverse_document_frequency[current_word_from_input];
-    }
-
-    map<string, double> normalized_tf_input;
-    for (long long i = 0; i < total_given_words; ++i)
-        normalized_tf_input[terms[i]] += 1;
-
-    map<string, double>:: iterator input_words;
-    for (input_words = normalized_tf_input.begin(); input_words != normalized_tf_input.end(); ++input_words)
-    {
-        double count = input_words->second;
-        input_words->second = count / double(total_given_words);
-    }
-
-    vector<double> tf_idf_input(total_given_words);
-    for (long long i = 0; i < total_given_words; ++i)
-    {
-        string current_word_from_input = terms[i];
-        tf_idf_input[i] = normalized_tf_input[current_word_from_input] * inverse_document_frequency[current_word_from_input];
-    }
-
-    vector<double> final_scores(total_no_of_files, 0);
-    for (long long i = 0; i < total_no_of_files; ++i)
-    {
-        double magnitude_document = 0, magnitude_query = 0;
-        for (long long j = 0; j < total_given_words; ++j)
-        {
-            final_scores[i] += (tf_idf_documents[j][i] * tf_idf_input[j]);
-            magnitude_document += (tf_idf_documents[j][i] * tf_idf_documents[j][i]);
-            magnitude_query += (tf_idf_input[j] * tf_idf_input[j]);
+            string current_word_from_input = terms[i];
+            for (long long j = 0; j < total_no_of_files; ++j)
+                tf_idf_documents[i][j] = normalized_tf_documents[j][current_word_from_input] * inverse_document_frequency[current_word_from_input];
         }
 
-        if (magnitude_document != 0)
-            final_scores[i] = final_scores[i] / (sqrt(magnitude_document) * sqrt(magnitude_query)); 
-    }
+        map<string, double> normalized_tf_input;
+        for (long long i = 0; i < total_given_words; ++i)
+            normalized_tf_input[terms[i]] += 1;
 
-    double max_value = *max_element(final_scores.begin(), final_scores.end());
-    cout << "\nRelevant Documents : " << endl;
-    for (long long i = 0; i < total_no_of_files; ++i)
-    {
-        if (final_scores[i] == max_value)
-            cout << file_names[i] << endl;
+        map<string, double>:: iterator input_words;
+        for (input_words = normalized_tf_input.begin(); input_words != normalized_tf_input.end(); ++input_words)
+        {
+            double count = input_words->second;
+            input_words->second = count / double(total_given_words);
+        }
+
+        vector<double> tf_idf_input(total_given_words);
+        for (long long i = 0; i < total_given_words; ++i)
+        {
+            string current_word_from_input = terms[i];
+            tf_idf_input[i] = normalized_tf_input[current_word_from_input] * inverse_document_frequency[current_word_from_input];
+        }
+
+        vector<double> final_scores(total_no_of_files, 0);
+        for (long long i = 0; i < total_no_of_files; ++i)
+        {
+            double magnitude_document = 0, magnitude_query = 0;
+            for (long long j = 0; j < total_given_words; ++j)
+            {
+                final_scores[i] += (tf_idf_documents[j][i] * tf_idf_input[j]);
+                magnitude_document += (tf_idf_documents[j][i] * tf_idf_documents[j][i]);
+                magnitude_query += (tf_idf_input[j] * tf_idf_input[j]);
+            }
+
+            if (magnitude_document != 0)
+                final_scores[i] = final_scores[i] / (sqrt(magnitude_document) * sqrt(magnitude_query)); 
+        }
+
+        double max_value = *max_element(final_scores.begin(), final_scores.end());
+        cout << "Relevant Documents : " << endl;
+        for (long long i = 0; i < total_no_of_files; ++i)
+        {
+            if (float(final_scores[i]) == float(max_value))
+                cout << file_names[i] << endl;
+        }
     }
     
     return 0;
